ca69ec89cb06ec623808f034e54403cd65375922
rishabhparsediya7 <parsediyarishabh@gmail.com>
2025-01-09 02:11:03 +0530

    axios instance created

---
+++ src/axios/index.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://1304-223-190-80-158.ngrok-free.app',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.error('Error attaching token:', error);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.error('Unauthorized - Redirect to login or refresh token');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
